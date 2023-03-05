44f15fd12530350ff01e11db5e1e9d03ec5ec340
Sabyasachi Sarkar <54432341+sabyasachi073@users.noreply.github.com>
2023-03-05 11:38:55 +0000

    Added html and css

--- app.py
+++ app.py
@@ -1,41 +1,18 @@
-# Module always have lowercase
+from flask import Flask, render_template
+# To use the template we use the render_template function from flask
 
-from flask import Flask  # Here we are importing the class Flask from the flask package
-
-# Here we are importing functionalities of flask and insert it into the "app" variable
-app = Flask(__name__)  # Creating flask application with a name
-'''
-Within any Python script we have a variable named, "__name__". It indicates how a script was invoked.
-
-
-When we run the file using the "python" command than we will get "__name__" as "__main__" 
-
-print(__name__) 
---> O/P: __main__
-'''
-'''
-After creating an appication we add routes to it. We will tell the python when a url is requested.
-
-In a URL the part after the domain name is called as "route" or "path name".
-Ex: If URL--> www.abc.com/learn
-Here, "/learn" part is called route
-'''
-
-# Registering route
-'''
-Here '@' called decorators which is used for special functionalities.
-'''
+app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return "Hello Sabya"
+  return render_template("home.html")
 
-
-'''
-By using the decorater above, we have informed flask that when the url with "/" route is accessed than show "Hello World"
-'''
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
-  # To run the app locally we will
+'''
+In flask we can access any file stored in static folder by giving the path-> /static/filename
+
+Anything that we put inside the static folder is called an asset
+'''
