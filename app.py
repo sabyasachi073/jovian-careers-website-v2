3f4dccb9ba688ef50540eba93f861db520b54ba8
Sabyasachi Sarkar <54432341+sabyasachi073@users.noreply.github.com>
2023-03-13 12:47:21 +0000

    Store submitted application in database dynamically

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
@@ -14,20 +14,50 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(jobs)
-  
-'''
-Here we are getting the "id" of the job from the URL. If anything with route as "/jobs/<id>" is searched than the variable "id" will store the content after "/jobs/". 
-Example: If the route is "jobs/3" than the variable "id" will be equal to 3.
-'''
-@app.route("/job/<id>") 
+
+
+@app.route("/job/<id>")
 def show_job(id):
   job = load_job_from_db(id)
-  
+
   if not job:
-    # Sending the Error message with the error code or status
-    return "Not Found", 404 
-    
-  return render_template("jobpage.html", job = job);
+    return "Not Found", 404
+
+  return render_template("jobpage.html", job=job)
+
+
+'''
+@app.route("/job/<id>/apply")
+def applt_to_job(id):
+  # In the url what ever is present after '?' that can be accessed using "request" variable in flask
+  data = request.args 
+  
+  # The data that we received can be used to ->
+  #   > Store in the database
+  #   > Send an email
+  #   > Display an acknowledgement
+  
+  return jsonify(data)
+'''
+'''
+When a route used the method "post" than the route "/job/<id>/apply" expects data to be posted by the browser and not sent in the url. When the method is 'post' than nothing is captured in "request.args", as "request.args" captures the data from URL. The information submitted in the form is captured in the "request.form".
+
+When we send by post than nothing is present in URL shown in the URL but the information is posted.
+'''
+
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+
+  add_application_to_db(
+    id, data)  # It stores the application data into the database
+
+  return render_template('application_submitted.html',
+                         application=data,
+                         job=job)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -35,14 +35,34 @@ result = conn.execute(text("select * from jobs where id = :val"), val = id)
 
 , we are inserting the value of "val" from the parameter "id" that is available. In the query we are mentioning the variable 'val' in a special way and this is a special way of doing string formatting in sqlalchemy. The value of ':val' will be replaced with the value of 'id' coming from the parameter which is actually coming from the route .
 '''
+
+
 def load_job_from_db(id):
   with engine.connect() as conn:
-    result = conn.execute(
-      text("select * from jobs where id = :val"), {"val": id})
-    
+    result = conn.execute(text("select * from jobs where id = :val"),
+                          {"val": id})
+
     rows = result.all()
 
     if len(rows) == 0:
       return None
     else:
       return dict(rows[0]._mapping)
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VAlUES(:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+
+    conn.execute(
+      query, {
+        "job_id": job_id,
+        "full_name": data['full_name'],
+        "email": data['email'],
+        "linkedin_url": data['linkedin_url'],
+        "education": data['education'],
+        "work_experience": data['work_experience'],
+        "resume_url": data['resume_url']
+      })
